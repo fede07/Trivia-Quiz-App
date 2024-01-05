55ecd5b58dea950b56d67dd42ac8ff6e53c5bef2
Federico Rossendy <fede07@users.noreply.github.com>
2024-01-05 19:55:10 -0300

    Day 35 completed

--- quiz_brain.py
+++ quiz_brain.py
@@ -2,11 +2,11 @@ from html import unescape
 
 class QuizBrain:
 
-    def __init__(self, q_list):
+    def __init__(self, q_list: list):
         self.question_number = 0
         self.score = 0
         self.question_list = q_list
-        self.current_question = None
+        self.current_question = self.question_list[self.question_number]
 
     def still_has_questions(self):
         return self.question_number < len(self.question_list)
@@ -15,16 +15,12 @@ class QuizBrain:
         self.current_question = self.question_list[self.question_number]
         self.question_number += 1
         q_text = unescape(self.current_question.text)
-        user_answer = input(f"Q.{self.question_number}: {q_text} (True/False): ")
-        self.check_answer(user_answer)
+        return f"Q.{self.question_number}: {q_text} (True/False): "
 
     def check_answer(self, user_answer):
         correct_answer = self.current_question.answer
         if user_answer.lower() == correct_answer.lower():
             self.score += 1
-            print("You got it right!")
+            return True
         else:
-            print("That's wrong.")
-
-        print(f"Your current score is: {self.score}/{self.question_number}")
-        print("\n")
+            return False
--- ui.py
+++ ui.py
@@ -1,29 +1,73 @@
+from cgitb import text
 from tkinter import Canvas, Label, Tk, Button, PhotoImage
+from quiz_brain import QuizBrain
 
-FONT = ("Arial", 20, "italic")
+FONT = ("Arial", 16, "italic")
 THEME_COLOR = "#375362"
 
 class QuizInterface:
 
-    def __init__(self) -> None:
-        self.score = 0
+    def __init__(self, quiz_brain: QuizBrain) -> None:
+        self.quiz = quiz_brain
         self.window=Tk()
         self.window.title("Quizzler")
         self.window.config(padx=20, pady=20,background=THEME_COLOR)
 
         self.canvas = Canvas(height=250, width=300, bg="white", highlightthickness=0)
-        self.question_text = self.canvas.create_text(150,125, text="Placeholder Text", font=FONT)
+        self.question_text = self.canvas.create_text(
+            150,
+            125, 
+            width=280,
+            text="Placeholder Text", 
+            font=FONT
+        )
         self.canvas.grid(column=0, row=1, columnspan=2, pady=50)
 
-        self.score_label = Label(text=f"Score: {self.score}", background=THEME_COLOR, fg="white")
+        self.score_label = Label(text=f"Score: {self.quiz.score}", background=THEME_COLOR, fg="white")
         self.score_label.grid(column=1, row=0)
 
         self.true_image = PhotoImage(file=r"images\true.png")
-        self.true_button = Button(image=self.true_image, highlightthickness=0)
+        self.true_button = Button(image=self.true_image, highlightthickness=0, command=self.true_pressed)
         self.true_button.grid(column=0, row=2)
 
         self.false_image = PhotoImage(file=r"images\false.png")
-        self.false_button = Button(image=self.false_image, highlightthickness=0)
+        self.false_button = Button(image=self.false_image, highlightthickness=0, command=self.false_pressed)
         self.false_button.grid(column=1, row=2)
 
+        self.get_next_question()
+        
         self.window.mainloop()
+        
+    def get_next_question(self):
+        self.canvas.config(bg="white")
+        if self.quiz.still_has_questions():
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text = q_text)
+            self.score_label.config(text=f"Score: {self.quiz.score}")
+            self.enable_buttons()
+        else:
+            self.canvas.itemconfig(self.question_text, text="You have reached the end of the quiz")
+            self.disable_buttons()
+            
+    def true_pressed(self):
+        self.give_feedback(self.quiz.check_answer("true"))
+        self.disable_buttons()
+        
+    def false_pressed(self):
+        self.give_feedback(self.quiz.check_answer("false"))    
+        self.disable_buttons()
+        
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
+        self.window.after(1000, self.get_next_question)
+        
+    def disable_buttons(self):
+        self.true_button.config(state="disabled")
+        self.false_button.config(state="disabled")
+        
+    def enable_buttons(self):
+        self.true_button.config(state="active")
+        self.false_button.config(state="active")
